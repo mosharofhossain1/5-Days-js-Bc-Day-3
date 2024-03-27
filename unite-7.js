8babdd130f6f94da57616d36c6d0a9e69e6728cd
Mosharof <md5470415@gmail.com>
2024-03-27 12:04:33 +0600

    some array methods practice

--- unite-7.js
+++ unite-7.js
@@ -1,2 +1,35 @@
-console.log('unite seven is comming')
-console.log('new repo create day 3')
+// Array Methods Practice
+
+// 1. #creating an array
+/* Var arrayName = [ element, element …. ]
+Var arrayName = new Array()
+#check an array
+Array.isArray() 
+ArrayName instanceof Array;
+#accessing specific array elements by index
+arrayName[index]
+#converting an array into a string */
+
+const fruits = ['banana', 'apple', 'jackfruit', 'Orange'];
+
+// 2. Push, POp, Shift, Unshift 
+
+// fruits.push('bedena');
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// fruits.unshift('pianapple');
+// console.log(fruits);
+
+
+// 3.arrayName.splice()
+console.log(fruits.splice(1,1));
+console.log(fruits);
+
+// 4. arrayName.toString()
+
+console.log(fruits.toString());
+
+// 5. 
